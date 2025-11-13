4affe8d05f958bd3d3d7053dd211c110e9cfd538
Kurt <jfmanalac@up.edu.ph>
2025-11-13 19:11:32 +0800

    script update

--- install-files.sh
+++ install-files.sh
@@ -2,8 +2,12 @@
 
 curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
 bash nodesource_setup.sh
-apt install -y nodejs
+apt install -y nodejs &
+INSTALL_PID=$!
+wait $INSTALL_PID
 
-npm install --prefix ./node-api --production
+npm install --prefix ./node-api --production &
+NPM_PID=$!
+wait $NPM_PID
 
 node /app/node-api/app.js
