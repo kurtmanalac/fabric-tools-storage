9af6c3436d30123d54bc83f41537b70435dc6326
Kurt <jfmanalac@up.edu.ph>
2025-11-13 19:08:14 +0800

    test

--- install-files.sh
+++ install-files.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
-sudo -E bash nodesource_setup.sh
-sudo apt install -y nodejs
+bash nodesource_setup.sh
+apt install -y nodejs
 
 npm install --prefix ./node-api --production
 
