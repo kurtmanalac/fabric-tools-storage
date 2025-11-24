0d68b0c3bfbd44ccef64051990c7cf4a78f7b537
Kurt <jfmanalac@up.edu.ph>
2025-11-24 16:11:11 +0800

    test fix

--- node-api/transfer-file.sh
+++ node-api/transfer-file.sh
@@ -23,6 +23,8 @@ curl -o $SOURCE_FOLDER/$FOLDER_NAME.zip $SOURCE_URL$SOURCE_FOLDER.zip
 # COPY_PID=$!
 # wait $COPY_PID
 
+sleep 5
+
 # echo "deleting zip file from $source..."
 # curl -X POST $SOURCE_URL/invoke-script \
 #     -H "Content-Type: application/json" \
