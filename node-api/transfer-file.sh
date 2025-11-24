0d9304d5e32ff23ba1f443fa613b11497d83adef
Kurt <jfmanalac@up.edu.ph>
2025-11-24 16:33:47 +0800

    test fix

--- node-api/transfer-file.sh
+++ node-api/transfer-file.sh
@@ -19,7 +19,7 @@ curl -X POST $SOURCE_URL/zip-folder \
 sleep 5
 
 mkdir -p $SOURCE_FOLDER
-curl -o $SOURCE_FOLDER/$FOLDER_NAME.zip $SOURCE_URL$SOURCE_FOLDER.zip 
+curl -f -# -o $SOURCE_FOLDER/$FOLDER_NAME.zip $SOURCE_URL$SOURCE_FOLDER.zip 
 # COPY_PID=$!
 # wait $COPY_PID
 
